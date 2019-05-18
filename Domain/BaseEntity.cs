04e8b984379738a5e8392ee4e2c330f18613d856
stefan-jevtic <sjevtic14@gmail.com>
2019-05-18 15:39:48 +0200

    Initial commit

---
+++ Domain/BaseEntity.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Domain
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
---
+++ Domain/City.cs
@@ -0,0 +1,7 @@
+namespace Domain
+{
+    public class City : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
