cc92e250c23cf8484fce53a853a66ad059789d43
stefan-jevtic <sjevtic14@gmail.com>
2019-05-19 15:07:56 +0200

    Login logic implemented

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -1,9 +1,14 @@
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using Application.DTO;
 using Domain;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Repository.Repositories;
 using Repository.UnitOfWork;
 
@@ -14,10 +19,12 @@ namespace API.Controllers
     public class AuthController : Controller
     {
         private IUnitOfWork _unitOfWork;
+        private IConfiguration _config;
         
-        public AuthController(IUnitOfWork unitOfWork)
+        public AuthController(IUnitOfWork unitOfWork, IConfiguration config)
         {
             _unitOfWork = unitOfWork;
+            _config = config;
         }
         
         [HttpPost]
@@ -57,6 +64,40 @@ namespace API.Controllers
             
             return Ok("User successfully registried!");
         }
+
+        [Authorize]
+        [HttpGet]
+        [Route("Zasticena")]
+        public IActionResult Zasticena()
+        {
+            return Ok("Proso si");
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login([FromBody] AuthDTO data)
+        {
+            if (String.IsNullOrEmpty(data.Email))
+            {
+                return BadRequest("Email field is required!");
+            }
+            
+            if (String.IsNullOrEmpty(data.Password))
+            {
+                return BadRequest("Password field is required!");
+            }
+            
+            if (!data.Email.Contains("@"))
+            {
+                return BadRequest("Enter valid email!");
+            }
+            
+            
+
+            var token = GenerateJSONWebToken(data);
+            
+            return Ok(token);
+        }
         
         static string ComputeSha256Hash(string rawData)  
         {  
@@ -74,6 +115,24 @@ namespace API.Controllers
                 }  
                 return builder.ToString();  
             }  
+        }  
+        private string GenerateJSONWebToken(AuthDTO dto)  
+        {  
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));  
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);  
+            
+            var claims = new[] {  
+                new Claim(JwtRegisteredClaimNames.Email, dto.Email),  
+                new Claim(JwtRegisteredClaimNames.Sid, dto.Password)
+            }; 
+  
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],  
+                _config["Jwt:Issuer"],  
+                null,  
+                expires: DateTime.Now.AddMinutes(120),  
+                signingCredentials: credentials);  
+  
+            return new JwtSecurityTokenHandler().WriteToken(token);  
         }  
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using DataAccess;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using Repository.Interfaces;
 using Repository.Repositories;
 using Repository.UnitOfWork;
@@ -33,6 +36,20 @@ namespace API
             services.AddTransient<IUserRepository, UserRepository>();
             services.AddTransient<IUnitOfWork, UnitOfWork>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)  
+                .AddJwtBearer(options =>  
+                {  
+                    options.TokenValidationParameters = new TokenValidationParameters  
+                    {  
+                        ValidateIssuer = true,  
+                        ValidateAudience = true,  
+                        ValidateLifetime = true,  
+                        ValidateIssuerSigningKey = true,  
+                        ValidIssuer = Configuration["Jwt:Issuer"],  
+                        ValidAudience = Configuration["Jwt:Issuer"],  
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))  
+                    };  
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +66,7 @@ namespace API
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
