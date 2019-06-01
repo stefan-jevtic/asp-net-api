a342d635e239d1e6f3f227859af0fb052f2e82d3
stefan-jevtic <sjevtic14@gmail.com>
2019-06-01 14:02:01 +0200

    Mailer finished

---
+++ Application/DTO/MailDTO.cs
@@ -0,0 +1,7 @@
+namespace Application.DTO
+{
+    public class MailDTO
+    {
+        
+    }
+}
---
+++ Application/Services/Implementation/Mailer.cs
@@ -0,0 +1,7 @@
+namespace Application.Services.Implementation
+{
+    public class Mailer
+    {
+        
+    }
+}
